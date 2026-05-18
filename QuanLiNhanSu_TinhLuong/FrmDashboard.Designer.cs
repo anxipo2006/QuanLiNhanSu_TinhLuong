d1b33b86516d30ca5777ba05095b5b529560e310
XoaiHvy <nhhson246@gmail.com>
2026-05-18 17:04:48 +0700

    fix

--- QuanLiNhanSu_TinhLuong/FrmDashboard.Designer.cs
+++ QuanLiNhanSu_TinhLuong/FrmDashboard.Designer.cs
@@ -28,53 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDashboard));
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges27 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges28 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges25 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges26 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges31 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges32 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges29 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges30 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges35 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges36 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges33 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges34 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             btnChamCong = new Guna.UI2.WinForms.Guna2Button();
             btnQuanLyNhanVien = new Guna.UI2.WinForms.Guna2Button();
             btnTinhLuong = new Guna.UI2.WinForms.Guna2Button();
             lblHeader = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            cardNhanSu = new Guna.UI2.WinForms.Guna2Panel();
-            picNhanSu = new Guna.UI2.WinForms.Guna2PictureBox();
-            lblNhanSuTitle = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            lblNhanSuDesc = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            cardChamCong = new Guna.UI2.WinForms.Guna2Panel();
-            picChamCong = new Guna.UI2.WinForms.Guna2PictureBox();
-            lblChamCongTitle = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            lblChamCongDesc = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            cardBangLuong = new Guna.UI2.WinForms.Guna2Panel();
-            picBangLuong = new Guna.UI2.WinForms.Guna2PictureBox();
-            lblBangLuongTitle = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            lblBangLuongDesc = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            cardNhanSu.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)picNhanSu).BeginInit();
-            cardChamCong.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)picChamCong).BeginInit();
-            cardBangLuong.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)picBangLuong).BeginInit();
             SuspendLayout();
             // 
             // btnChamCong
             // 
             btnChamCong.BorderRadius = 20;
-            btnChamCong.CustomizableEdges = customizableEdges19;
+            btnChamCong.CustomizableEdges = customizableEdges1;
             btnChamCong.DisabledState.BorderColor = Color.DarkGray;
             btnChamCong.DisabledState.CustomBorderColor = Color.DarkGray;
             btnChamCong.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -85,17 +55,16 @@
             btnChamCong.ImageSize = new Size(60, 60);
             btnChamCong.Location = new Point(371, 195);
             btnChamCong.Name = "btnChamCong";
-            btnChamCong.ShadowDecoration.CustomizableEdges = customizableEdges20;
+            btnChamCong.ShadowDecoration.CustomizableEdges = customizableEdges2;
             btnChamCong.Size = new Size(252, 88);
             btnChamCong.TabIndex = 0;
             btnChamCong.Text = "Chấm Công";
-            btnChamCong.Visible = false;
             btnChamCong.Click += btnChamCong_Click;
             // 
             // btnQuanLyNhanVien
             // 
             btnQuanLyNhanVien.BorderRadius = 20;
-            btnQuanLyNhanVien.CustomizableEdges = customizableEdges21;
+            btnQuanLyNhanVien.CustomizableEdges = customizableEdges3;
             btnQuanLyNhanVien.DisabledState.BorderColor = Color.DarkGray;
             btnQuanLyNhanVien.DisabledState.CustomBorderColor = Color.DarkGray;
             btnQuanLyNhanVien.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -106,17 +75,16 @@
             btnQuanLyNhanVien.ImageSize = new Size(60, 60);
             btnQuanLyNhanVien.Location = new Point(12, 195);
             btnQuanLyNhanVien.Name = "btnQuanLyNhanVien";
-            btnQuanLyNhanVien.ShadowDecoration.CustomizableEdges = customizableEdges22;
+            btnQuanLyNhanVien.ShadowDecoration.CustomizableEdges = customizableEdges4;
             btnQuanLyNhanVien.Size = new Size(252, 88);
             btnQuanLyNhanVien.TabIndex = 1;
             btnQuanLyNhanVien.Text = "Quản lý Nhân sự";
-            btnQuanLyNhanVien.Visible = false;
             btnQuanLyNhanVien.Click += btnQuanLyNhanVien_Click;
             // 
             // btnTinhLuong
             // 
             btnTinhLuong.BorderRadius = 20;
-            btnTinhLuong.CustomizableEdges = customizableEdges23;
+            btnTinhLuong.CustomizableEdges = customizableEdges5;
             btnTinhLuong.DisabledState.BorderColor = Color.DarkGray;
             btnTinhLuong.DisabledState.CustomBorderColor = Color.DarkGray;
             btnTinhLuong.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -127,11 +95,10 @@
             btnTinhLuong.ImageSize = new Size(60, 60);
             btnTinhLuong.Location = new Point(724, 195);
             btnTinhLuong.Name = "btnTinhLuong";
-            btnTinhLuong.ShadowDecoration.CustomizableEdges = customizableEdges24;
+            btnTinhLuong.ShadowDecoration.CustomizableEdges = customizableEdges6;
             btnTinhLuong.Size = new Size(252, 88);
             btnTinhLuong.TabIndex = 2;
             btnTinhLuong.Text = "Bảng Lương";
-            btnTinhLuong.Visible = false;
             btnTinhLuong.Click += btnTinhLuong_Click;
             // 
             // lblHeader
@@ -145,196 +112,19 @@
             lblHeader.TabIndex = 3;
             lblHeader.Text = "HỆ THỐNG QUẢN LÝ TỔNG THỂ";
             // 
-            // cardNhanSu
-            // 
-            cardNhanSu.BackColor = Color.Transparent;
-            cardNhanSu.BorderRadius = 20;
-            cardNhanSu.Controls.Add(picNhanSu);
-            cardNhanSu.Controls.Add(lblNhanSuTitle);
-            cardNhanSu.Controls.Add(lblNhanSuDesc);
-            cardNhanSu.Cursor = Cursors.Hand;
-            cardNhanSu.CustomizableEdges = customizableEdges27;
-            cardNhanSu.FillColor = Color.FromArgb(50, 110, 190);
-            cardNhanSu.Location = new Point(1002, 100);
-            cardNhanSu.Name = "cardNhanSu";
-            cardNhanSu.ShadowDecoration.BorderRadius = 20;
-            cardNhanSu.ShadowDecoration.CustomizableEdges = customizableEdges28;
-            cardNhanSu.ShadowDecoration.Depth = 15;
-            cardNhanSu.ShadowDecoration.Enabled = true;
-            cardNhanSu.Size = new Size(250, 280);
-            cardNhanSu.TabIndex = 4;
-            cardNhanSu.Click += btnQuanLyNhanVien_Click;
-            // 
-            // picNhanSu
-            // 
-            picNhanSu.BackColor = Color.Transparent;
-            picNhanSu.CustomizableEdges = customizableEdges25;
-            picNhanSu.ImageRotate = 0F;
-            picNhanSu.Location = new Point(250, 20);
-            picNhanSu.Name = "picNhanSu";
-            picNhanSu.ShadowDecoration.CustomizableEdges = customizableEdges26;
-            picNhanSu.Size = new Size(60, 60);
-            picNhanSu.SizeMode = PictureBoxSizeMode.Zoom;
-            picNhanSu.TabIndex = 0;
-            picNhanSu.TabStop = false;
-            // 
-            // lblNhanSuTitle
-            // 
-            lblNhanSuTitle.BackColor = Color.Transparent;
-            lblNhanSuTitle.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
-            lblNhanSuTitle.ForeColor = Color.White;
-            lblNhanSuTitle.Location = new Point(10, 95);
-            lblNhanSuTitle.Name = "lblNhanSuTitle";
-            lblNhanSuTitle.Size = new Size(180, 33);
-            lblNhanSuTitle.TabIndex = 1;
-            lblNhanSuTitle.Text = "<center>Quản lý Nhân sự</center>";
-            // 
-            // lblNhanSuDesc
-            // 
-            lblNhanSuDesc.BackColor = Color.Transparent;
-            lblNhanSuDesc.Font = new Font("Segoe UI", 10F);
-            lblNhanSuDesc.ForeColor = Color.LightGray;
-            lblNhanSuDesc.Location = new Point(10, 135);
-            lblNhanSuDesc.Name = "lblNhanSuDesc";
-            lblNhanSuDesc.Size = new Size(297, 25);
-            lblNhanSuDesc.TabIndex = 2;
-            lblNhanSuDesc.Text = "<center>Hồ sơ nhân viên, Tuyển dụng, Đào tạo</center>";
-            // 
-            // cardChamCong
-            // 
-            cardChamCong.BackColor = Color.Transparent;
-            cardChamCong.BorderRadius = 20;
-            cardChamCong.Controls.Add(picChamCong);
-            cardChamCong.Controls.Add(lblChamCongTitle);
-            cardChamCong.Controls.Add(lblChamCongDesc);
-            cardChamCong.Cursor = Cursors.Hand;
-            cardChamCong.CustomizableEdges = customizableEdges31;
-            cardChamCong.FillColor = Color.FromArgb(50, 110, 190);
-            cardChamCong.Location = new Point(1002, 100);
-            cardChamCong.Name = "cardChamCong";
-            cardChamCong.ShadowDecoration.BorderRadius = 20;
-            cardChamCong.ShadowDecoration.CustomizableEdges = customizableEdges32;
-            cardChamCong.ShadowDecoration.Depth = 15;
-            cardChamCong.ShadowDecoration.Enabled = true;
-            cardChamCong.Size = new Size(250, 280);
-            cardChamCong.TabIndex = 5;
-            cardChamCong.Click += btnChamCong_Click;
-            // 
-            // picChamCong
-            // 
-            picChamCong.BackColor = Color.Transparent;
-            picChamCong.CustomizableEdges = customizableEdges29;
-            picChamCong.ImageRotate = 0F;
-            picChamCong.Location = new Point(250, 20);
-            picChamCong.Name = "picChamCong";
-            picChamCong.ShadowDecoration.CustomizableEdges = customizableEdges30;
-            picChamCong.Size = new Size(60, 60);
-            picChamCong.SizeMode = PictureBoxSizeMode.Zoom;
-            picChamCong.TabIndex = 0;
-            picChamCong.TabStop = false;
-            // 
-            // lblChamCongTitle
-            // 
-            lblChamCongTitle.BackColor = Color.Transparent;
-            lblChamCongTitle.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
-            lblChamCongTitle.ForeColor = Color.White;
-            lblChamCongTitle.Location = new Point(10, 95);
-            lblChamCongTitle.Name = "lblChamCongTitle";
-            lblChamCongTitle.Size = new Size(126, 33);
-            lblChamCongTitle.TabIndex = 1;
-            lblChamCongTitle.Text = "<center>Chấm Công</center>";
-            // 
-            // lblChamCongDesc
-            // 
-            lblChamCongDesc.BackColor = Color.Transparent;
-            lblChamCongDesc.Font = new Font("Segoe UI", 10F);
-            lblChamCongDesc.ForeColor = Color.LightGray;
-            lblChamCongDesc.Location = new Point(10, 135);
-            lblChamCongDesc.Name = "lblChamCongDesc";
-            lblChamCongDesc.Size = new Size(317, 25);
-            lblChamCongDesc.TabIndex = 2;
-            lblChamCongDesc.Text = "<center>Theo dõi thời gian, Chuyên cần, Đi muộn</center>";
-            // 
-            // cardBangLuong
-            // 
-            cardBangLuong.BackColor = Color.Transparent;
-            cardBangLuong.BorderRadius = 20;
-            cardBangLuong.Controls.Add(picBangLuong);
-            cardBangLuong.Controls.Add(lblBangLuongTitle);
-            cardBangLuong.Controls.Add(lblBangLuongDesc);
-            cardBangLuong.Cursor = Cursors.Hand;
-            cardBangLuong.CustomizableEdges = customizableEdges35;
-            cardBangLuong.FillColor = Color.FromArgb(50, 110, 190);
-            cardBangLuong.Location = new Point(1002, 100);
-            cardBangLuong.Name = "cardBangLuong";
-            cardBangLuong.ShadowDecoration.BorderRadius = 20;
-            cardBangLuong.ShadowDecoration.CustomizableEdges = customizableEdges36;
-            cardBangLuong.ShadowDecoration.Depth = 15;
-            cardBangLuong.ShadowDecoration.Enabled = true;
-            cardBangLuong.Size = new Size(250, 280);
-            cardBangLuong.TabIndex = 6;
-            cardBangLuong.Click += btnTinhLuong_Click;
-            // 
-            // picBangLuong
-            // 
-            picBangLuong.BackColor = Color.Transparent;
-            picBangLuong.CustomizableEdges = customizableEdges33;
-            picBangLuong.ImageRotate = 0F;
-            picBangLuong.Location = new Point(250, 20);
-            picBangLuong.Name = "picBangLuong";
-            picBangLuong.ShadowDecoration.CustomizableEdges = customizableEdges34;
-            picBangLuong.Size = new Size(60, 60);
-            picBangLuong.SizeMode = PictureBoxSizeMode.Zoom;
-            picBangLuong.TabIndex = 0;
-            picBangLuong.TabStop = false;
-            // 
-            // lblBangLuongTitle
-            // 
-            lblBangLuongTitle.BackColor = Color.Transparent;
-            lblBangLuongTitle.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
-            lblBangLuongTitle.ForeColor = Color.White;
-            lblBangLuongTitle.Location = new Point(10, 95);
-            lblBangLuongTitle.Name = "lblBangLuongTitle";
-            lblBangLuongTitle.Size = new Size(133, 33);
-            lblBangLuongTitle.TabIndex = 1;
-            lblBangLuongTitle.Text = "<center>Bảng Lương</center>";
-            // 
-            // lblBangLuongDesc
-            // 
-            lblBangLuongDesc.BackColor = Color.Transparent;
-            lblBangLuongDesc.Font = new Font("Segoe UI", 10F);
-            lblBangLuongDesc.ForeColor = Color.LightGray;
-            lblBangLuongDesc.Location = new Point(10, 135);
-            lblBangLuongDesc.Name = "lblBangLuongDesc";
-            lblBangLuongDesc.Size = new Size(271, 25);
-            lblBangLuongDesc.TabIndex = 2;
-            lblBangLuongDesc.Text = "<center>Tính toán lương, Phụ cấp, Khấu trừ</center>";
-            // 
             // FrmDashboard
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(1002, 507);
+            ClientSize = new Size(991, 507);
             Controls.Add(lblHeader);
-            Controls.Add(cardNhanSu);
-            Controls.Add(cardChamCong);
-            Controls.Add(cardBangLuong);
-            Controls.Add(btnTinhLuong);
             Controls.Add(btnQuanLyNhanVien);
             Controls.Add(btnChamCong);
+            Controls.Add(btnTinhLuong);
             Name = "FrmDashboard";
             Text = "FrmDashboard";
             Load += FrmDashboard_Load;
-            cardNhanSu.ResumeLayout(false);
-            cardNhanSu.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)picNhanSu).EndInit();
-            cardChamCong.ResumeLayout(false);
-            cardChamCong.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)picChamCong).EndInit();
-            cardBangLuong.ResumeLayout(false);
-            cardBangLuong.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)picBangLuong).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -345,17 +135,5 @@
         private Guna.UI2.WinForms.Guna2Button btnQuanLyNhanVien;
         private Guna.UI2.WinForms.Guna2Button btnTinhLuong;
         private Guna.UI2.WinForms.Guna2HtmlLabel lblHeader;
-        private Guna.UI2.WinForms.Guna2Panel cardNhanSu;
-        private Guna.UI2.WinForms.Guna2Panel cardChamCong;
-        private Guna.UI2.WinForms.Guna2Panel cardBangLuong;
-        private Guna.UI2.WinForms.Guna2PictureBox picNhanSu;
-        private Guna.UI2.WinForms.Guna2HtmlLabel lblNhanSuTitle;
-        private Guna.UI2.WinForms.Guna2HtmlLabel lblNhanSuDesc;
-        private Guna.UI2.WinForms.Guna2PictureBox picChamCong;
-        private Guna.UI2.WinForms.Guna2HtmlLabel lblChamCongTitle;
-        private Guna.UI2.WinForms.Guna2HtmlLabel lblChamCongDesc;
-        private Guna.UI2.WinForms.Guna2PictureBox picBangLuong;
-        private Guna.UI2.WinForms.Guna2HtmlLabel lblBangLuongTitle;
-        private Guna.UI2.WinForms.Guna2HtmlLabel lblBangLuongDesc;
     }
 }
